b07edcd1ef9de0ea80d97edc0796e6ba57e083a0
PrasadParanjape07 <83603078+PrasadParanjape07@users.noreply.github.com>
2025-08-04 10:23:32 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -7,7 +7,7 @@ def print_color_map():
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
 
-
 result = print_color_map()
 assert(result == 25)
 print("All is well (maybe!)")
+
